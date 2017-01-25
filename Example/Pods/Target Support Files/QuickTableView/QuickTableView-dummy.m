eb969a42f4e9a0363da2f63b2f74cc59842807ad
Dan Loewenherz <dan@lionheartsw.com>
2017-01-24 19:24:05 -0600

    update pod target support files

--- Example/Pods/Target Support Files/QuickTableView/QuickTableView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QuickTableView : NSObject
-@end
-@implementation PodsDummy_QuickTableView
-@end
--- Example/Pods/Target Support Files/QuickTableView/QuickTableView-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double QuickTableViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char QuickTableViewVersionString[];
-
