570f174597136134b212b7e7349682259a529777
Dan Loewenherz <dan@lionheartsw.com>
2017-01-24 19:21:44 -0600

    update bump_version

--- bump_version.sh
+++ bump_version.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
+  git tag -d $1
   sed -i "" "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" QuickTableView.podspec
-  sed -i "" "s/tree\/[\.0-9]*/tree\/$1/g" generate_docs.sh
-  sed -i "" "s/module-version [\.0-9]*/module-version $1/g" generate_docs.sh
   git add .
   git commit -m "bump version to $1"
   git tag $1
   git push origin master
-  git push --tags
+  git push --tags --force
   pod trunk push
 fi
