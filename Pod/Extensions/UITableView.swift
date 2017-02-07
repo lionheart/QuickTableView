753e876a097f191d4518ff33c76da9e3d28e47d7
Dan Loewenherz <dan@lionheartsw.com>
2017-02-07 15:16:22 -0600

    use concrete protocol types instead of conformance

--- Pod/Extensions/UITableView.swift
+++ Pod/Extensions/UITableView.swift
@@ -14,13 +14,13 @@
 //  limitations under the License.
 
 public extension UITableView {
-    func registerClasses<T: QuickTableViewCellIdentifiableFixedHeight>(_ cellClasses: [T.Type]) {
+    func registerClasses(_ cellClasses: [QuickTableViewCellIdentifiableFixedHeight.Type]) {
         for cellClass in cellClasses {
             registerClass(cellClass)
         }
     }
 
-    func registerClasses<T: QuickTableViewCellIdentifiableAutomaticHeight>(_ cellClasses: [T.Type], withEstimatedRowHeight estimatedRowHeight: CGFloat = 44) {
+    func registerClasses(_ cellClasses: [QuickTableViewCellIdentifiableAutomaticHeight.Type], withEstimatedRowHeight estimatedRowHeight: CGFloat = 44) {
         for cellClass in cellClasses {
             registerClass(cellClass)
         }
